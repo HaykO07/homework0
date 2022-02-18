db72af91c26d3c58c4fc40fe199504af42d0289a
HaykO07 <scott.gambler26@gmail.com>
2022-02-18 17:23:35 +0400

    homework2

--- homework2.py
+++ homework2.py
@@ -1,2 +1,15 @@
-# homework0
-homework
+x=0
+z=0
+quantity=0
+while x!="done":
+    
+    x=input("Please enter your number: ")
+    if x.isdecimal():
+      
+        quantity=quantity+1
+        z=int(x)+z
+    if x.isdecimal()==False and x!="done":
+        print('Please enter only numbers or string "done"')
+print('Summary is',z)    
+print("Quantity is",quantity)    
+print('Arithmetic average is',z/quantity)
